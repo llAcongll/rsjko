9b83084fe6ddf9116b7aa27b6e7e981e86b0f3c0
llAcongll <ariansyah06.smk5@gmail.com>
2026-03-03 01:07:10 +0700

    Update title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>SIPP BLUD EHD</title>
+  <title>SIK BLUD EHD</title>
 
   {{-- FAVICON & PWA --}}
   <link rel="icon" type="image/png" href="{{ asset('favicon-light.png') }}?v=1.2" media="(prefers-color-scheme: light)">
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>SIPP BLUD EHD</title>
+  <title>SIK BLUD EHD</title>
 
   {{-- FAVICON & PWA --}}
   <!-- Light mode -->
